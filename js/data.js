b606e82e8cd8c5e5b715969970da3d4bddec6aa3
onhq11 <mateuszmatecki25@gmail.com>
2023-04-05 08:49:01 +0200

    Indev v0.0.2
    Screen quality changed

--- js/data.js
+++ js/data.js
@@ -14,68 +14,153 @@ let oobeContentTab = [
     </p>`,
 
     `<div class="map" style="height: 11.5%; width: 4.2%; top: 7.25%; left: 57.25%"></div>
-    <img src="./img/windows/1.png" class="windowSS">`,
+    <img src="./img/windows/1.png" class="windowSS">
+    <div class="hint">
+        <div></div>
+        <h1>Krok 1</h1>
+        <h2>Utwórz nową maszynę, klikając na górze przycisk "Nowa"</h2>
+    </div>`,
 
     `<img src="./img/windows/1.png" class="windowSS">
-    <img src="./img/windows/2.png" class="windowSS popupCenter">`,
+    <img src="./img/windows/2.png" class="windowSS popupCenter">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 2</h1>
+        <h2>Uzupełnij pole "Nazwa" i wybierz na dole typ systemu (*Kliknij dowolny przycisk*)</h2>
+    </div>`,
 
     `<div class="map imBlue" style="height: 3.25%; width: 6.1%; top: 74.3%; left: 66.25%"></div>
     <img src="./img/windows/1.png" class="windowSS">
-    <img src="./img/windows/3.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">`,
+    <img src="./img/windows/3.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 3</h1>
+        <h2>Kliknij przycisk "Next"</h2>
+    </div>`,
 
     `<img src="./img/windows/1.png" class="windowSS">
-    <img src="./img/windows/4.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">`,
+    <img src="./img/windows/4.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 4</h1>
+        <h2>Ustaw pamięć RAM i ilość rdzeni tak, aby spełniały minimalne wymagania systemu</h2>
+    </div>`,
 
     `<div class="map imBlue" style="height: 3.25%; width: 6.1%; top: 74.3%; left: 66.25%"></div>
     <img src="./img/windows/1.png" class="windowSS">
-    <img src="./img/windows/5.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">`,
+    <img src="./img/windows/5.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 5</h1>
+        <h2>Kliknij przycisk "Next"</h2>
+    </div>`,
 
     `<div class="map imBlue" style="height: 3.25%; width: 6.1%; top: 74.3%; left: 66.25%"></div>
     <img src="./img/windows/1.png" class="windowSS">
-    <img src="./img/windows/6.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">`,
+    <img src="./img/windows/6.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 6</h1>
+        <h2>Tutaj można ustawić rozmiar dysku, jeżeli potrzeba. (zostawiamy domyślnie)</h2>
+    </div>`,
 
     `<div class="map imBlue" style="height: 3.25%; width: 6.1%; top: 74.3%; left: 66.25%"></div>
-    <img src="./img/windows/1.png" class="windowSS">
-    <img src="./img/windows/7.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">`,
+    <img src="./img/windows/1.png" class="windowSS">    
+    <img src="./img/windows/7.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 7</h1>
+        <h2>Wyświetla się nam podsumowanie, jeżeli wszystko się zgadza klikamy przycisk "Zakończ"</h2>
+    </div>`,
 
     `<div class="map" style="height: 11.5%; width: 7.2%; top: 7.25%; left: 41.9%;"></div>
-    <img src="./img/windows/8.png" class="windowSS">`,
+    <img src="./img/windows/8.png" class="windowSS">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 8</h1>
+        <h2>Teraz edytujemy ustawienia maszyny, aby dodać plik ISO systemu. Klikamy w zębatkę na górze na pasku.</h2>
+    </div>`,
 
     `<div class="map" style="height: 5%; width: 11.5%; top: 32.25%; left: 20.75%;"></div>
     <img src="./img/windows/8.png" class="windowSS">
-    <img src="./img/windows/9.png" class="windowSS popupCenter">`,
+    <img src="./img/windows/9.png" class="windowSS popupCenter">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 9</h1>
+        <h2>Kliknij w zakładkę "Ekran"</h2>
+    </div>`,
 
     `<img src="./img/windows/8.png" class="windowSS">
-    <img src="./img/windows/10.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">`,
+    <img src="./img/windows/10.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 10</h1>
+        <h2>Ustaw tutaj suwak na 128MB</h2>
+    </div>`,
 
     `<div class="map" style="height: 5%; width: 11.5%; top: 34.25%; left: 20.75%;"></div>
     <img src="./img/windows/8.png" class="windowSS">
-    <img src="./img/windows/11.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">`,
+    <img src="./img/windows/11.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 11</h1>
+        <h2>Przejdź do zakładki "Pamięć"</h2>
+    </div>`,
 
     `<div class="map imBlue" style="height: 3.5%; width: 16%; top: 32.5%; left: 34.25%;"></div>
     <img src="./img/windows/8.png" class="windowSS">
-    <img src="./img/windows/12.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">`,
+    <img src="./img/windows/12.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 12</h1>
+        <h2>Klikamy na płytę po prawej stronie w liście</h2>
+    </div>`,
 
     `<div class="map" style="height: 3.5%; width: 1.75%; top: 28.75%; left: 77.5%;"></div>
     <img src="./img/windows/8.png" class="windowSS">
-    <img src="./img/windows/13.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">`,
+    <img src="./img/windows/13.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 13</h1>
+        <h2>Następnie klikamy po prawej stronie w ikonę płyty</h2>
+    </div>`,
 
     `<div class="map" style="height: 3.5%; width: 21%; top: 34.75%; left: 58.2%;"></div>
     <img src="./img/windows/8.png" class="windowSS">
-    <img src="./img/windows/14.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">`,
+    <img src="./img/windows/14.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 14</h1>
+        <h2>Klikamy na pole "Wybierz plik dysku"</h2>
+    </div>`,
 
     `<div class="map imBlue" style="height: 2.5%; width: 38%; top: 38.4%; left: 31.2%;"></div>
     <div class="map imBlue" style="height: 3%; width: 5.5%; top: 77.25%; left: 67.2%;"></div>
     <img src="./img/windows/8.png" class="windowSS">
     <img src="./img/windows/13.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">
-    <img src="./img/windows/15.png" class="windowSS popupCenter">`,
+    <img src="./img/windows/15.png" class="windowSS popupCenter">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 15</h1>
+        <h2>Wybieramy pobrany plik ISO systemu</h2>
+    </div>`,
 
     `<div class="map imBlue" style="height: 3.5%; width: 5.75%; top: 80.5%; left: 61%;"></div>
     <img src="./img/windows/8.png" class="windowSS">
-    <img src="./img/windows/16.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">`,
+    <img src="./img/windows/16.png" class="windowSS popupCenter" style="opacity: 1; width: 60%;">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 16</h1>
+        <h2>Zatwierdzamy klikając OK</h2>
+    </div>`,
 
     `<div class="map" style="height: 11.5%; width: 5.5%; top: 7.25%; left: 54%;"></div>
-    <img src="./img/windows/8.png" class="windowSS">`,
+    <img src="./img/windows/8.png" class="windowSS">
+    <div class="hint" style="opacity: 0.0; bottom: -30px">
+        <div></div>
+        <h1>Krok 17</h1>
+        <h2>Uruchamiamy maszynę na górze przyciskiem "Uruchom"</h2>
+    </div>`,
 
     `<img src="./img/windows/17.png" class="windowSS">`
 ]
--- js/oobe.js
+++ js/oobe.js
@@ -6,6 +6,14 @@ const fakeCursor = () => {
 }
 
 const changeOOBESize = (sizes) => {
+    if(window.innerHeight < 450) {
+        window.location.href = 'unsupported.html'
+    }
+
+    if(window.innerWidth < window.innerHeight / 2) {
+        window.location.href = 'unsupported.html'
+    }
+
     if(sizes.w !== 0) {
         oobeWindow.style.width = sizes.w+'%'
     } else {
@@ -21,85 +29,99 @@ const changeOOBESize = (sizes) => {
 
 const replaceOOBEContent = (content, playerRunning, openPopup) => {
     if(playerRunning) {
-        oobeContent.innerHTML = content
+        const playerHint = document.getElementsByClassName('hint')[0]
+        playerHint.style.bottom = '-30px'
+        playerHint.style.opacity = 0.0
 
         setTimeout(() => {
-            if(openPopup) {
-                popupFade(true)
-            }
+            document.getElementsByClassName('hint')[0].style.bottom = '30px'
+            document.getElementsByClassName('hint')[0].style.opacity = 1.0
 
-            if(oobeStep === 3) {
-                cursorInterval = setInterval(fakeCursor, 500)
-            } else {
-                clearInterval(cursorInterval)
-            }
-
-            if(oobeStep === 8 || oobeStep === 17) {
-                document.getElementsByClassName('map')[0].addEventListener('click', () => {
-                    popupFade(false)
-                    document.getElementsByClassName('map')[0].style.display = 'none'
-
-                    setTimeout(() => {
+            oobeContent.innerHTML = content
+    
+            setTimeout(() => {
+                if(openPopup) {
+                    popupFade(true)
+                }
+    
+                if(oobeStep === 3) {
+                    cursorInterval = setInterval(fakeCursor, 500)
+                } else {
+                    clearInterval(cursorInterval)
+                }
+    
+                if(oobeStep === 8 || oobeStep === 17) {
+                    document.getElementsByClassName('map')[0].addEventListener('click', () => {
+                        popupFade(false)
+                        document.getElementsByClassName('map')[0].style.display = 'none'
+    
+                        setTimeout(() => {
+                            oobeStep++
+                            replaceOOBEContent(oobeContentTab[oobeStep], true)
+                        }, 300)
+                    })
+                } else if(oobeStep === 18) {
+                    document.getElementsByClassName('oobeContent')[0].style.transition = '0.2s'
+                    changeOOBESize(oobeContentSize[oobeStep])
+    
+                    document.getElementsByClassName('map')[0].addEventListener('click', () => {
+                        document.getElementsByClassName('oobeContent')[0].style.opacity = 0.0
+                        changeOOBESize(oobeContentSize[oobeStep+1])
+    
+                        setTimeout(() => {
+                            oobeStep++
+                            replaceOOBEContent(oobeContentTab[oobeStep], true)
+    
+                            document.getElementsByClassName('oobeContent')[0].style.opacity = 1.0
+                        }, 500)
+                        
+                    })
+                } else if(oobeStep === 5 || oobeStep === 11) {
+                    document.getElementsByClassName('popupCenter')[0].addEventListener('click', () => {
+                        oobeStep++
+                        replaceOOBEContent(oobeContentTab[oobeStep], true, false)
+                    })
+                } else if(oobeStep === 9 || oobeStep === 2 || oobeStep === 15) {
+                    document.getElementsByClassName('map')[0].addEventListener('click', () => {
+                        oobeStep++
+                        replaceOOBEContent(oobeContentTab[oobeStep], true, true)
+                    })
+                } else if(oobeStep === 16) {
+                    document.getElementsByClassName('map')[0].addEventListener('click', () => {
+                        popupFade(false)
+                        document.getElementsByClassName('map')[0].style.display = 'none'
+    
+                        setTimeout(() => {
+                            oobeStep++
+                            replaceOOBEContent(oobeContentTab[oobeStep], true)
+                        }, 300)
+                    })
+    
+                    document.getElementsByClassName('map')[1].addEventListener('click', () => {
+                        popupFade(false)
+                        document.getElementsByClassName('map')[1].style.display = 'none'
+    
+                        setTimeout(() => {
+                            oobeStep++
+                            replaceOOBEContent(oobeContentTab[oobeStep], true)
+                        }, 300)
+                    })
+                } else if((oobeStep === 4 || oobeStep > 5) && document.getElementsByClassName('map').length > 0) {
+                    document.getElementsByClassName('map')[0].addEventListener('click', () => {
                         oobeStep++
                         replaceOOBEContent(oobeContentTab[oobeStep], true)
-                    }, 300)
-                })
-            } else if(oobeStep === 18) {
-                document.getElementsByClassName('oobeContent')[0].style.transition = '0.2s'
-                changeOOBESize(oobeContentSize[oobeStep])
-
-                document.getElementsByClassName('map')[0].addEventListener('click', () => {
-                    document.getElementsByClassName('oobeContent')[0].style.opacity = 0.0
-                    changeOOBESize(oobeContentSize[oobeStep+1])
-
-                    setTimeout(() => {
-                        oobeStep++
-                        replaceOOBEContent(oobeContentTab[oobeStep], true)
-
-                        document.getElementsByClassName('oobeContent')[0].style.opacity = 1.0
-                    }, 500)
-                    
-                })
-            } else if(oobeStep === 9 || oobeStep === 2 || oobeStep === 15) {
-                document.getElementsByClassName('map')[0].addEventListener('click', () => {
-                    oobeStep++
-                    replaceOOBEContent(oobeContentTab[oobeStep], true, true)
-                })
-            } else if(oobeStep === 16) {
-                document.getElementsByClassName('map')[0].addEventListener('click', () => {
-                    popupFade(false)
-                    document.getElementsByClassName('map')[0].style.display = 'none'
-
-                    setTimeout(() => {
-                        oobeStep++
-                        replaceOOBEContent(oobeContentTab[oobeStep], true)
-                    }, 300)
-                })
-
-                document.getElementsByClassName('map')[1].addEventListener('click', () => {
-                    popupFade(false)
-                    document.getElementsByClassName('map')[1].style.display = 'none'
-
-                    setTimeout(() => {
-                        oobeStep++
-                        replaceOOBEContent(oobeContentTab[oobeStep], true)
-                    }, 300)
-                })
-            } else if((oobeStep === 4 || oobeStep > 5) && document.getElementsByClassName('map').length > 0) {
-                document.getElementsByClassName('map')[0].addEventListener('click', () => {
-                    oobeStep++
-                    replaceOOBEContent(oobeContentTab[oobeStep], true)
-                })
-            }
-            
-            if(oobeStep === 3 || oobeStep === 5 || oobeStep == 10 || oobeStep === 19) {
-                dataFilled = false
-            }
-
-            if(document.getElementsByClassName('map').length > 0) {
-                document.getElementsByClassName('map')[0].style.display = 'block'
-            }
-        }, 10)
+                    })
+                }
+                
+                if(oobeStep === 3 || oobeStep === 19) {
+                    dataFilled = false
+                }
+    
+                if(document.getElementsByClassName('map').length > 0) {
+                    document.getElementsByClassName('map')[0].style.display = 'block'
+                }
+            }, 10)
+        }, 1000)
         return
     }
 
